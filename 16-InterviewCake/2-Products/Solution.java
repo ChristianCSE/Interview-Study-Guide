0e6adbec4103b2a4c78c6d90e3ed246cd565b2a8
ChristianCSE <csanch4@emory.edu>
2017-07-23 16:24:18 -0700

    rm ahead arr

--- 16-InterviewCake/2-Products/Solution.java
+++ 16-InterviewCake/2-Products/Solution.java
@@ -6,7 +6,9 @@ class Solution {
   public static int[] productArrays(int[] arr) {
     int len = arr.length;
     List<Integer> behind = new ArrayList<>();
+    
     List<Integer> ahead = new ArrayList<>();
+    int aheadProd = 1;
     int[] prodArr = new int[len];
     int prod = 1;
     for(int i = 0; i < len-1; i++) {
@@ -16,17 +18,26 @@ class Solution {
     prodArr[len-1] = prod;
     behind.remove(behind.size()-1); //remove last el which is our next eval
     ahead.add(arr[len-1]); //add our curr element since ahead of next el
+    
+    aheadProd*=arr[len-1];
+
     for(int i = len -2; i >= 0; i--) {
       System.out.println("->" + prod);
       prod = 1;
       for(Integer back: behind){
         prod*= back;
       }
+
       if(!behind.isEmpty()) behind.remove(behind.size()-1); //remove upcoming from behind
+      
+      /*
       for(Integer front: ahead) {
         prod*= front;
       }
       ahead.add(arr[i]); //you will be ahead of upcoming 
+      */
+      prod *= aheadProd;
+      aheadProd*=arr[i];
 
       prodArr[i] = prod;
     }
