bb13072d8c63bd7b0be3689ba120a228c65c600d
ChristianCSE <csanch4@emory.edu>
2017-07-22 20:20:19 -0700

    pure words complete

--- 15-Prep/snapchat/Snap/WordBreak.java
+++ 15-Prep/snapchat/Snap/WordBreak.java
@@ -6,12 +6,42 @@ import java.util.*;
 class WordBreak {
 
 
-  
+  public static List<String> pureWords(String[] words) {
+    List<String> reference = new ArrayList<>(); 
+    for(String word: words) {
+      reference.add(word);
+    }
+    List<String> pure = new ArrayList<>();
+    for(String candidate: reference) {
+      boolean store = isPure(candidate, reference);
+      if(store) pure.add(candidate);
+    }
+
+    return pure;
+  }
+
+
+  public static boolean isPure(String candidate, List<String> dict) {
+    boolean[] substringCache = new boolean[candidate.length()+1];
+    substringCache[0] = true;
+    for(int i = 1; i <= candidate.length(); i++) {
+      for(int j = 0; j < i; j++) {
+        //.substring(j, i)
+        if( substringCache[j] && dict.contains(candidate.substring(j, i)) && !candidate.substring(j, i).equals(candidate)) {
+          System.out.println("=>" + candidate + " ===>  " + candidate.substring(j, i));
+          substringCache[i] = true;
+          break;
+        }
+      }
+    }
+    System.out.println(substringCache[candidate.length()] + "    ===" + candidate);
+    return !substringCache[candidate.length()];
+  }
 
   public static void main(String[] args) {
     String[] dict = new String[]{"chat","ever","snapchat","snap","salesperson","per","person","sales","son","whatsoever","what","so"};
-
-
+    List<String> pure = pureWords(dict);
+    System.out.print(pure);
 
 
     //output:
