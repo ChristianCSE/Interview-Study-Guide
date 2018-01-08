5b5c3a0a2c6e24780f8e20439c3da48c24f874f1
ChristianCSE <csanch4@emory.edu>
2018-01-07 19:33:35 -0800

    paritioning it or color sorting

--- 1-Sorting-Algorithms/Partition_Sort/Solution.java
+++ 1-Sorting-Algorithms/Partition_Sort/Solution.java
@@ -1,11 +1,11 @@
-
-
-
+//This isn't necessarily completely sorting a given array; rather, 
+//it is distributing the array around a pivot. 
+//Result: [less than or equal to pivot, pivot, greater than pivot ]
 import java.util.Arrays;
 class Partition {
   //everything less than the kth largest goes to the left of it
   //everything greater than the kth largest goes to the right of it
-  public static int placePivot(int[] array, int kth){
+  public static int sort(int[] array, int kth){
     int right = array.length - 1;
     //kth = nth highest, if you are 1st highest that means you are 
     //greater than/equal to = totalValues-1
@@ -13,18 +13,18 @@ class Partition {
     System.out.println("Before: "+Arrays.toString(array));
     int numberKth = getKth(array, 0, right, kth);
     System.out.println("After:  "+Arrays.toString(array));
+    System.out.println("kth:" + numberKth);
     return numberKth;
   }
 
   public static int getKth(int[] array, int left, int right, int k){
-    if(left==right) return nums[left];
+    if(left==right) return array[left];
     int parition = parition(array, left, right);
     
     if(parition == k) return array[parition];
     else if(parition > k) return getKth(array, left, parition-1, k);
     //
-    else return getKth(array, left, parition+1, k)
-    return 0;
+    else return getKth(array, left, parition+1, k);
   }
   
   /*
@@ -39,14 +39,36 @@ class Partition {
   That's why when we split the array we do parition (+/-) 1!
    */
   public static int parition(int[] array, int left, int right){
-    
-    return 0;
-  } 
+    int pivot = array[right];
+    for(int i = left; i < right; i++){
+      //this is color sorting: [less than equal to pivot, pivot, greater than pivot]
+      if(array[i] <= pivot){
+        swap(array, left, i);
+        left++;
+      }
+    }
+    /*
+    NOTE: all elements to the LEFT of our LEFT pointer are less than or equal to 
+    our pivot. 
+    LEFT is the index of where our current pivot should be placed, it's also 
+    the value of the number of values it's greater than or equal to (aka kth).
+     */
+    swap(array, left, right);
+    return left;
+  }
+
+  public static void swap(int[] array, int i, int j){
+    int temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
 }
 
 
 class Solution {
   public static void main(String[] args) {
-    
+    int[] array = {10, 8, 20, 40, 4, 18, 22, 50};
+    //4, 8, 10, 18, 20, 22, 40, 50
+    Partition.sort(array, 3); //3 = 22
   }
 }
